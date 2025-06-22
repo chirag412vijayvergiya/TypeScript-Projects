ebef6e87d8ca29e64d1f924ee957dd30595eb0fd
Chirag vijayvergiya <111374446+chirag412vijayvergiya@users.noreply.github.com>
2025-06-22 11:20:56 +0530

    UseReducer

--- vite-react-ts-project/src/App.tsx
+++ vite-react-ts-project/src/App.tsx
@@ -4,6 +4,8 @@
 // import List from "./components/List";
 // import { useState } from "react";
 
+import Counter from "./components/Counter"
+
 // function App() {
 //   const [count, setCount] = useState(1);
 
@@ -22,66 +24,78 @@
 
 // export default App
 
-import { useCallback, useEffect, useMemo, useRef, useState, type KeyboardEvent, type MouseEvent } from "react";
+// import { useCallback, useEffect, useMemo, useRef, useState, type KeyboardEvent, type MouseEvent } from "react";
 
-interface Users {
-  id: number,
-  name: string
-}
+// interface Users {
+//   id: number,
+//   name: string
+// }
 
-type fibFunc = (n: number) => number 
+// type fibFunc = (n: number) => number 
 
-const fib: fibFunc = (n) => {
-  if (n < 2) return n 
-  return fib(n - 1) + fib(n - 2) 
-}
+// const fib: fibFunc = (n) => {
+//   if (n < 2) return n 
+//   return fib(n - 1) + fib(n - 2) 
+// }
 
-const myNum: number = 37 
+// const myNum: number = 37 
+
+// function App() {
+//   const [count, setCount] = useState(1);
+//   const [users, setUsers] = useState<Users[] | null>(null);
+
+//   const inputRef = useRef<HTMLInputElement>(null);
+
+//   console.log(inputRef?.current) 
+//   console.log(inputRef?.current?.value)
+
+//   useEffect(() => {
+//   const fetchedUsers = [
+//     { id: 1, name: "Chirag" },
+//     { id: 2, name: "John" },
+//   ];
+
+//   setUsers(fetchedUsers);
+// }, []);
+
+//   useEffect(() => {
+//     console.log("Mounting")
+//     console.log("Users :- ", users);
+
+//     return () => console.log("Unmounting");
+// }, [users])
+
+//   const addTwo = useCallback((e: MouseEvent<HTMLButtonElement> | KeyboardEvent<HTMLButtonElement> ): void => setCount(prev => prev + 2), []);
+
+//   const result = useMemo<number>(() => fib(myNum),[myNum])
+
+//   return (
+//     <div className="App">
+//       <h1>{count}</h1>
+//       <button onClick={addTwo}></button>
+//       <h2>{result}</h2>
+//       <input ref={inputRef} type="text" 
+//           onChange={() => {
+//             console.log("Input value:", inputRef.current?.value);
+//           }}/>
+//       {users?.map((user, i) => (
+//             <li key={i}>
+//                 <p>{user.name} with {user.id}</p>
+//             </li>
+//         ))}
+//     </div>
+//   )
+// }
+
+// export default App
+
+
 
 function App() {
-  const [count, setCount] = useState(1);
-  const [users, setUsers] = useState<Users[] | null>(null);
-
-  const inputRef = useRef<HTMLInputElement>(null);
-
-  console.log(inputRef?.current) 
-  console.log(inputRef?.current?.value)
-
-  useEffect(() => {
-  const fetchedUsers = [
-    { id: 1, name: "Chirag" },
-    { id: 2, name: "John" },
-  ];
-
-  setUsers(fetchedUsers);
-}, []);
-
-  useEffect(() => {
-    console.log("Mounting")
-    console.log("Users :- ", users);
-
-    return () => console.log("Unmounting");
-}, [users])
-
-  const addTwo = useCallback((e: MouseEvent<HTMLButtonElement> | KeyboardEvent<HTMLButtonElement> ): void => setCount(prev => prev + 2), []);
-
-  const result = useMemo<number>(() => fib(myNum),[myNum])
-
   return (
-    <div className="App">
-      <h1>{count}</h1>
-      <button onClick={addTwo}></button>
-      <h2>{result}</h2>
-      <input ref={inputRef} type="text" 
-          onChange={() => {
-            console.log("Input value:", inputRef.current?.value);
-          }}/>
-      {users?.map((user, i) => (
-            <li key={i}>
-                <p>{user.name} with {user.id}</p>
-            </li>
-        ))}
-    </div>
+    <>
+    <Counter>{(num: number) => <>Current Count: {num}</>}</Counter>
+    </>
   )
 }
 
